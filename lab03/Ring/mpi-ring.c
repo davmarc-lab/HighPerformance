a33c25eac238fa27fcbae23bb25dd44cb7444334
Davide Marchetti <davide.marchetti6@studio.unibo.it>
2023-11-08 11:47:16 +0100

    started ring connection

--- lab03/Ring/mpi-ring.c
+++ lab03/Ring/mpi-ring.c
@@ -19,51 +19,51 @@
  ****************************************************************************/
 
 /***
-% HPC - Ring communication with MPI
-% Moreno Marzolla <moreno.marzolla@unibo.it>
-% Last updated: 2023-11-06
+  % HPC - Ring communication with MPI
+  % Moreno Marzolla <moreno.marzolla@unibo.it>
+  % Last updated: 2023-11-06
 
-Write a MPI program [mpi-ring.c](mpi-ring.c) that implements a message
-exchange along a ring. Let $P$ be the number of MPI processes; the
-program should behave according to the following specification:
+  Write a MPI program [mpi-ring.c](mpi-ring.c) that implements a message
+  exchange along a ring. Let $P$ be the number of MPI processes; the
+  program should behave according to the following specification:
 
-- The program receives an integer $K \geq 1$ from the command
+  - The program receives an integer $K \geq 1$ from the command
   line. $K$ is the number of "turns" of the ring. Since all MPI
   processes have access to the command line parameters, they know the
   value $K$ without the need to communicate.
 
-- Process 0 (the master) sends process 1 an integer, whose
+  - Process 0 (the master) sends process 1 an integer, whose
   initial value is 1.
 
-- Every process $p$ (including the master) receives a value $v$ from
+  - Every process $p$ (including the master) receives a value $v$ from
   the predecessor $p-1$, and sends $(v + 1)$ to the successor
   $p+1$. The predecessor of 0 is $(P - 1)$, and the successor of $(P -
   1)$ is 0.
 
-- The master prints the value received after the $K$-th iteration and
+  - The master prints the value received after the $K$-th iteration and
   the program terminates. Given the number $P$ of processes and the
   value of $K$, what value should be printed by the master?
 
-For example, if $K = 2$ and there are $P = 4$ processes, the
-communication should be as shown in Figure 1 (arrows are messages
-whose content is the number reported above them). There are $K = 2$
-turns of the ring; at the end, process 0 receives and prints 8.
+  For example, if $K = 2$ and there are $P = 4$ processes, the
+  communication should be as shown in Figure 1 (arrows are messages
+  whose content is the number reported above them). There are $K = 2$
+  turns of the ring; at the end, process 0 receives and prints 8.
 
-![Figure 1: Ring communication](mpi-ring.svg)
+  ![Figure 1: Ring communication](mpi-ring.svg)
 
-To compile:
+  To compile:
 
-        mpicc -std=c99 -Wall -Wpedantic mpi-ring.c -o mpi-ring
+  mpicc -std=c99 -Wall -Wpedantic mpi-ring.c -o mpi-ring
 
-To execute:
+  To execute:
 
-        mpirun -n 4 ./mpi-ring
+  mpirun -n 4 ./mpi-ring
 
 ## Files
 
 - [mpi-ring.c](mpi-ring.c)
 
-***/
+ ***/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -72,15 +72,38 @@ To execute:
 int main( int argc, char *argv[] )
 {
     int my_rank, comm_sz, K = 10;
+    int value = 1;
 
     MPI_Init( &argc, &argv );
     MPI_Comm_rank( MPI_COMM_WORLD, &my_rank );
     MPI_Comm_size( MPI_COMM_WORLD, &comm_sz );
 
+
     if ( argc > 1 ) {
         K = atoi(argv[1]);
     }
     /* [TODO] Rest of the code here... */
+
+    printf("comm size = %d\n", K);
+    if (my_rank == 0) 
+    {
+        MPI_Send(&value, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    }
+
+        const int pred = my_rank - 1 == 0 ? comm_sz : my_rank - 1;
+        const int succ = my_rank + 1 == comm_sz ? 0 : my_rank + 1;
+        printf("DEBUG: pred = %d, succ = %d\n", pred, succ);
+
+        MPI_Recv(&value, 1, MPI_INT, pred, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        value++;
+        printf("DEBUG: value = %d\n", value);
+        MPI_Send(&value, 1, MPI_INT, succ, 0, MPI_COMM_WORLD);
+
+    if (my_rank == 0)
+    {
+        // end of games
+        printf("End value = %d\n", value);
+    }
 
     MPI_Finalize();
     return EXIT_SUCCESS;
