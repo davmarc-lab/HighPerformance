5ff01c87f9326d1df7ac3fff39f0a9306c456805
Davide Marchetti <davide.marchetti6@studio.unibo.it>
2025-06-03 10:12:48 +0200

    sync

---
+++ Elaborato/src/omp-test-all.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+files=`ls ./datasets/*.in`
+
+ot="omp-out"
+if [ ! -e $ot ] ; then
+    mkdir $ot
+fi
+
+make omp
+
+rs="omp-result.txt"
+if [ -e $rs ] ; then
+    rm $rs
+fi
+touch $rs
+
+i=0
+for fin in $files ;
+do
+    ((i=$i + 1))
+    ./omp-skyline < $fin > $ot/$ot$i.txt
+    echo $fin >> $rs
+    head "$ot/$ot$i.txt" -n 2 >> $rs
+    echo "" >> $rs
+done
